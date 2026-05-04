c9be34655774986f511ad2ab72b8869b3451bd4e
dmdinteractive <aidan@dmdinteractive.com>
2026-05-03 19:29:57 -0600

    Particle random walk working - prints coordinates of each step

--- dla_simulation.py
+++ dla_simulation.py
@@ -1,2 +1,36 @@
-print(helloworld)
-dojsdojnfoajsnd;jnsd;jn f;gkj asdlkm gasdlkmf/lm welkn
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+grid_size = 300
+num_particles = 4000
+sticking_probality = 1.0 #sigma
+
+grid=np.zeros((grid_size,grid_size))
+
+print(grid_size)
+print(num_particles)
+print(sticking_probality)
+print(grid)
+
+center = grid_size // 2
+grid[center, center] = 1
+
+def move():
+    return random.choice([(-1,0),(1,0),(0,-1),(0,1)])
+
+x, y = center, center
+
+for step in range(10):
+         dx,dy = move()
+         x = x + dx
+         y = y + dy
+         print(x,y)
+
+
+plt.imshow(grid, cmap="gray")
+plt.show()
+
+
+
+
